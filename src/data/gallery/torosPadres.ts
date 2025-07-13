ca1356f3dd053e3a58bb30f7028d223e19c7bf0d
Idana Wagner Cabrera <idanawagner@gmail.com>
2025-07-13 04:01:52 -0300

    fix img

--- src/data/gallery/torosPadres.ts
+++ src/data/gallery/torosPadres.ts
@@ -1,42 +1,46 @@
+import eric from '@/assets/images/toros-padres/NEWHOUSE-JEWDOR-ERIC.png';
+import kingJazmin from '@/assets/images/toros-padres/CURAMALAN-KING-JAZMIN.png';
+import countVesuvius from '@/assets/images/toros-padres/CURAMALAN-COUNT-VESUVIS.png';
+
 export const torosPadres = [
     {
         id: 1,
-        image: '/src/assets/images/toros-padres/NEWHOUSE-JEWDOR-ERIC.png',
+        image: eric,
         title: 'NEWHOUSE JEWDOR ERIC',
         description:
             'Por Jewvil Eric of Manorhill y Jeweleva Erica por Ervil of Harviestoun, campeón de la Royal Highland de 1963.',
     },
     {
         id: 2,
-        image: '/src/assets/images/toros-padres/CURAMALAN-KING-JAZMIN.png',
+        image: kingJazmin,
         title: 'CURAMALAN KING JAZMIN',
         description:
             'Nació el 27 de noviembre de 1975. Adquirido particularmente en la cabaña Curamalan.',
     },
     {
         id: 3,
-        image: '/src/assets/images/toros-padres/CURAMALAN-COUNT-VESUVIS.png',
+        image: countVesuvius,
         title: 'CURAMALAN COUNT VESUVIUS',
         description:
             'Adquirido en 750.000 pesos precio máximo del remate anual de Curamalan. Nació el 5 de enero de 1977.',
     },
     {
         id: 4,
-        image: '/src/assets/images/toros-padres/NEWHOUSE-JEWDOR-ERIC.png',
+        image: eric,
         title: 'NEWHOUSE JEWDOR ERIC',
         description:
             'Por Jewvil Eric of Manorhill y Jeweleva Erica por Ervil of Harviestoun, campeón de la Royal Highland de 1963.',
     },
     {
         id: 5,
-        image: '/src/assets/images/toros-padres/CURAMALAN-KING-JAZMIN.png',
+        image: kingJazmin,
         title: 'CURAMALAN KING JAZMIN',
         description:
             'Nació el 27 de noviembre de 1975. Adquirido particularmente en la cabaña Curamalan.',
     },
     {
         id: 6,
-        image: '/src/assets/images/toros-padres/CURAMALAN-COUNT-VESUVIS.png',
+        image: countVesuvius,
         title: 'CURAMALAN COUNT VESUVIUS',
         description:
             'Adquirido en 750.000 pesos precio máximo del remate anual de Curamalan. Nació el 5 de enero de 1977.',
