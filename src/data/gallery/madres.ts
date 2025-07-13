4999c3fd61901209d9c6e9ef5809311b745e4372
Idana Wagner Cabrera <idanawagner@gmail.com>
2025-07-13 04:05:37 -0300

    FIX IMG

--- src/data/gallery/madres.ts
+++ src/data/gallery/madres.ts
@@ -1,19 +1,23 @@
+import ELABRALADYPRINCE2166 from '@/assets/images/madres/ELABRALADYPRINCE2166.png';
+import ELABRAQUEENIDA2404 from '@/assets/images/madres/ELABRAQUEENIDA2404.png';
+import SINIDENTIFICACION from '@/assets/images/madres/SINIDENTIFICACION.png';
+
 export const madres = [
     {
         id: 1,
-        image: '/src/assets/images/madres/ELABRALADYPRINCE2166.png',
+        image: ELABRALADYPRINCE2166,
         title: 'EL ABRA LADY PRINCE 2166',
         description: 'Hija de Witbert of Haymount.',
     },
     {
         id: 2,
-        image: '/src/assets/images/madres/ELABRAQUEENIDA2404.png',
+        image: ELABRAQUEENIDA2404,
         title: 'EL ABRA QUEEN IDA 2404 y una hermana paterna...',
         description: 'ambas hijas de Witbert of Haymount',
     },
     {
         id: 3,
-        image: '/src/assets/images/madres/SINIDENTIFICACION.png',
+        image: SINIDENTIFICACION,
         title: '',
         description: '',
     },
